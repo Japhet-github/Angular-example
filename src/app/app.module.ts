6976b04ed9c7c95f8a508f8b7d8e1eb7e0b5acdd
Japhet <VannJaphet@gmail.com>
2023-08-11 10:39:53 +0200

    imported HttpClientModule to consume the REST API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// imported HttpClientModule service to consume the REST API 
+// https://angular.io/api/common/http/HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
