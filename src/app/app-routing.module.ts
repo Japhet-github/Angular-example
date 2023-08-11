35bb244d2ed21add253421077ae574891cd70b7d
Japhet <VannJaphet@gmail.com>
2023-08-11 10:48:39 +0200

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// imported Angular components 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+// the first route is for redirecting the empty path to the home component,
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'}, // Home page to display when first visit. 
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
